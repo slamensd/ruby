0c4dcdc1bb482a937ff03caa2743c0d29440cef3
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-23 19:00:46 +0000

    cc

--- app.js
+++ app.js
@@ -47,34 +47,27 @@ window.onload = async () => {
         }
     });
 
-    // ...previous code
+    document.getElementById("verifyBtn").addEventListener("click", async () => {
+        try {
+            let tokenId = 6;
+            let balance = await contract.balanceOf(userAddress, tokenId);
+            console.log("Token balance:", balance.toString());
 
-document.getElementById('verifyBtn').addEventListener('click', async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const contract = new ethers.Contract(contractAddress, abi, provider);
-    try {
-        let balance = await contract.callStatic['balanceOf(address,uint256)'](walletAddress, tokenId);
-        balance = Number(balance);
-        if (balance > 0) {
-            document.getElementById('verificationMessage').innerText = 'Token verified!';
-            // update the reveal of the hidden message
-            document.getElementById('hiddenMessage').innerHTML = '<p class="hidden-message">We start at zero, book one unfurled,</p>'+
-            '<p class="hidden-message">Page one\'s second letter "U", into our world.</p>'+
-            '<p class="hidden-message">To page three we turn, the fifth letter to peek,</p>'+
-            '<p class="hidden-message">On this journey of ours, the clues we seek.</p>'+
-            '<p class="hidden-message">Fast forward to eight, a letter to meet,</p>'+
-            '<p class="hidden-message">Put them together, the word is complete.</p>'+
-            '<p class="hidden-message">A three-letter puzzle, for your mind\'s keep,</p>'+
-            '<p class="hidden-message">Seek, find, and unravel, in this mystery deep.</p>';
-        } else {
-            throw new Error('Token not found in this wallet!');
+            if (balance > 0) {
+                document.getElementById("verificationMessage").innerText = "Token Verified!";
+                document.getElementById("hiddenMessage").innerHTML = '<p class="hidden-message">We start at zero, book one unfurled,</p>'+
+                '<p class="hidden-message">Page one\'s second letter "U", into our world.</p>'+
+                '<p class="hidden-message">To page three we turn, the fifth letter to peek,</p>'+
+                '<p class="hidden-message">On this journey of ours, the clues we seek.</p>'+
+                '<p class="hidden-message">Fast forward to eight, a letter to meet,</p>'+
+                '<p class="hidden-message">Put them together, the word is complete.</p>'+
+                '<p class="hidden-message">A three-letter puzzle, for your mind\'s keep,</p>'+
+                '<p class="hidden-message">Seek, find, and unravel, in this mystery deep.</p>';
+            } else {
+                document.getElementById("verificationMessage").innerText = "Error: Token not found in this wallet!";
+            }
+        } catch (error) {
+            console.error("Error during token verification:", error);
         }
-    } catch (error) {
-        console.error("Failed to check the balance of the token:", error);
-        document.getElementById('verificationMessage').innerText = 'Error verifying token!';
-    }
-});
-
-// ...previous code
-
+    });
 };
