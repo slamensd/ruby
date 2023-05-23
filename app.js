f507f7203bdea61cdd524c34f8fe4b8f42f70185
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-23 18:08:37 +0000

    ff

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-// Establish connection to the provider.
-let provider;
-let userAddress;
-let signer;
+window.onload = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        console.log('MetaMask is installed!');
+    }
 
-document.getElementById("connectBtn").addEventListener("click", async () => {
-    if (window.ethereum) {
-        provider = new ethers.providers.Web3Provider(window.ethereum);
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let signer = provider.getSigner();
+    let userAddress;
+    let contractAddress = "0x232765be70a5f0b49e2d72eee9765813894c1fc4";
+
+    let erc1155Abi = [
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "name": "id",
+                    "type": "uint256"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+
+    let contract = new ethers.Contract(contractAddress, erc1155Abi, provider);
+
+    document.getElementById("connectBtn").addEventListener("click", async () => {
         try {
-            // Request account access
-            await window.ethereum.enable();
-
-            // Account access granted, get signer
-            signer = provider.getSigner();
-
-            // Get the user's address
-            userAddress = await signer.getAddress();
-            document.getElementById("walletAddress").innerText = `Wallet Address: ${userAddress}`;
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            userAddress = accounts[0];
+            document.getElementById("walletAddress").innerText = `Connected: ${userAddress}`;
+            console.log("Wallet connected:", userAddress);
         } catch (error) {
-            console.error("User denied account access:", error);
-            document.getElementById("walletAddress").innerText = "Error: User denied account access!";
+            console.error("Error connecting to wallet:", error);
         }
-    } else {
-        console.error("No Ethereum provider was found. Please install MetaMask.");
-        document.getElementById("walletAddress").innerText = "Error: No Ethereum provider was found!";
-    }
-});
+    });
 
-document.getElementById("verifyBtn").addEventListener("click", async () => {
-    const facetAddress = "0xcd3decb28dbfa49579237928c1a7df2687d88881";
-    const tokenId = 6;
+    document.getElementById("verifyBtn").addEventListener("click", async () => {
+        try {
+            let tokenId = 6;
+            let balance = await contract.balanceOf(userAddress, tokenId);
+            console.log("Token balance:", balance.toString());
 
-    try {
-        // Set up a call to the balanceOf function
-        const data = ethers.utils.defaultAbiCoder.encode(
-            ["address", "uint256"],
-            [userAddress, tokenId]
-        );
-
-        const transaction = {
-            to: facetAddress,
-            data: "0x00fdd58e" + data.slice(2), // prepend function selector
-        };
-
-        console.log("Calling contract with transaction:", transaction);
-
-        // Call balanceOf function using Diamond Standard contract
-        const result = await provider.call(transaction);
-        console.log("Received result from contract:", result);
-
-        // Convert hexadecimal balance to a decimal number
-        const balance = parseInt(result, 16);
-
-        console.log("Token balance:", balance);
-
-        if (balance > 0) {
-            document.getElementById("verificationMessage").innerText = "Token Verified!";
-            document.getElementById("hiddenMessage").innerText = "We start at zero, book one unfurled, Page one's second letter \"U\", into our world. To page three we turn, the fifth letter to peek, On this journey of ours, the clues we seek. Fast forward to eight, a letter to meet, Put them together, the word is complete. A three-letter puzzle, for your mind's keep, Seek, find, and unravel, in this mystery deep.";
-        } else {
-            document.getElementById("verificationMessage").innerText = "Error: Token not found in this wallet!";
+            if (balance > 0) {
+                document.getElementById("verificationMessage").innerText = "Token Verified!";
+                document.getElementById("hiddenMessage").innerText = "We start at zero, book one unfurled, Page one's second letter \"U\", into our world. To page three we turn, the fifth letter to peek, On this journey of ours, the clues we seek. Fast forward to eight, a letter to meet, Put them together, the word is complete. A three-letter puzzle, for your mind's keep, Seek, find, and unravel, in this mystery deep.";
+            } else {
+                document.getElementById("verificationMessage").innerText = "Error: Token not found in this wallet!";
+            }
+        } catch (error) {
+            console.error("Error during token verification:", error);
         }
-    } catch (error) {
-        console.error("Error during token verification:", error);
-    }
-});
-
+    });
+};
